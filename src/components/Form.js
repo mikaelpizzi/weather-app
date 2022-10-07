4975a10e62f86c065f3425fd811a5e997b94f04a
Mikael Pizzi <mikaelangelopizzi@gmail.com>
2022-10-06 22:15:02 -0400

    [ADD] Add form state to process data

--- src/components/Form.js
+++ src/components/Form.js
@@ -7,6 +7,9 @@ const Form = (props) => {
         country: ''
     });
 
+    // Error state
+    const [error, saveError] = useState(false);
+
     // Extract city and country
     const { city, country } = search;
 
@@ -19,8 +22,23 @@ const Form = (props) => {
         })
     }
 
+    // When user submits form
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // Validate
+        if(city.trim() === '' || country.trim() === '') {
+            saveError(true);
+            return;
+        }
+
+        saveError(false);
+    }
     return ( 
-        <form>
+        <form
+            onSubmit={handleSubmit}
+        >
+            { error ? <p className='red darken-4 error'>All fields are required</p> : null }
             <div className='input-field col s12'>
                 <input
                     type='text'
@@ -56,6 +74,17 @@ const Form = (props) => {
                 </select>
                 
                 <label>Country: </label>
+
+                <div className="input-field col s12">
+                    <button 
+                        type="submit"
+                        name="action"
+                        className="waves-effect waves-light btn-large btn-block yellow accent-4 black-text"
+                        style={{width: "100%", fontWeight: "600"}}
+                    >
+                    Search Weather
+                    </button> 
+                </div>
             </div>
         </form>
      );
